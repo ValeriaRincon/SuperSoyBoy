212b6c09adcc85d12024331214e9864f9a2caa4c
Valeria Rincon <v.rincon558@gmail.com>
2021-12-02 14:01:13 -0500

    Ch. 13 - Added GameManager and level goal

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -40,6 +40,7 @@ public class Hazard : MonoBehaviour
             spriteRenderer.sprite = hitSprite;
 
             Destroy(coll.gameObject);
+            GameManager.instance.RestartLevel(1.25f);
         }
     }
 }
